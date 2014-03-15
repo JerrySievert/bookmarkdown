8a838055a11d1032aa813d6e55a77ffdd849f57d
Jerry Sievert <code@legitimatesounding.com>
2014-03-15 15:48:40 -0700

    better formatting of hrefs

--- test/html-test.js
+++ test/html-test.js
@@ -14,7 +14,7 @@ vows.describe('HTML').addBatch({
       return results;
     },
     'the html should be correct': function (topic) {
-      var expected = "<div class=\"toc\">\n  <ul><li>0. <a href=\"#Chapter_1\">Chapter 1</a></li><li>1. <a href=\"#Chapter_2\">Chapter 2</a></li></ul>\n</div>";
+      var expected = "<div class=\"toc\">\n  <ul><li>0. <a href=\"#chapter-1\">Chapter 1</a></li><li>1. <a href=\"#chapter-2\">Chapter 2</a></li></ul>\n</div>";
       assert.equal(topic, expected);
     }
   },
@@ -24,7 +24,7 @@ vows.describe('HTML').addBatch({
       return results;
     },
     'the html should be correct': function (topic) {
-      var expected = "<div class=\"chapter\">\n  <a link=\"Chapter_1\"></a>\n  <h2 class=\"chapter-title\">Chapter 1</h2>\n  <div class=\"chapter-text\">\n    <p>In the beginning, there was the book.</p>\n\n  </div>\n</div>";
+      var expected = "<div class=\"chapter\">\n  <a link=\"chapter-1\"></a>\n  <h2 class=\"chapter-title\">Chapter 1</h2>\n  <div class=\"chapter-text\">\n    <p>In the beginning, there was the book.</p>\n\n  </div>\n</div>";
 
       assert.equal(topic, expected);
     }
